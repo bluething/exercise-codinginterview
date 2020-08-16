12fdc76e5fef24223b87f50c0594be32a7e3a35b
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-08-16 16:35:53 +0700

    1st solution
    My idea is find the largest value, keep index and value.
    Then iterate the array, in each iteration check if the current index not same with max index and max value less than 2*current index value. If true return -1, means the answer is false, largest element us "at least twice".

---
+++ leetcode/learn/src/test/java/io/github/bluething/leetcode/learn/arrayandstring/LargestNumberAtLeastTwiceofOthersTest.java
@@ -0,0 +1,33 @@
+package io.github.bluething.leetcode.learn.arrayandstring;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/*
+* The problem https://leetcode.com/explore/learn/card/array-and-string/201/introduction-to-array/1147/
+* */
+public class LargestNumberAtLeastTwiceofOthersTest {
+
+    @Test
+    public void successWhenInputIsRight() {
+        Assert.assertEquals(1, dominantIndex(new int[]{3, 6, 1, 0}));
+    }
+
+    private int dominantIndex(int[] nums) {
+        int maxValue = 0;
+        int maxIndex = 0;
+        for (int i=0; i < nums.length; i++) {
+            if(nums[i] >= maxValue) {
+                maxValue = nums[i];
+                maxIndex = i;
+            }
+        }
+        for (int i=0; i < nums.length; i++) {
+            if(i != maxIndex && maxValue < 2 * nums[i]) {
+                return -1;
+            }
+        }
+        return maxIndex;
+    }
+
+}
