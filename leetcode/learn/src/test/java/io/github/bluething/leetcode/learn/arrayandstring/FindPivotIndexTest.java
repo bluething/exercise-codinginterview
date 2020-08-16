9a74abc3fb7b761a6816f58ff2d1f00bf6eaae9d
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-08-16 15:28:25 +0700

    1st solution
    Failed for some test cases. There are flaw in my algorithm, my assumption is sum left is sum value in left pivot candidate and right sum is sum value in right pivot candidate. So the pivot candidate between nums[1] and nums[nums.lenght()-2]
    
    Until I hit case [-1,-1,-1,0,1,1]
    Answer for case is [0], because sum right is 0, then sum left is 0. Why sum left is 0? I think this is a assumption from the creator. In real interview, we must always ask for assumption from the interviewer!

---
+++ leetcode/learn/src/test/java/io/github/bluething/leetcode/learn/arrayandstring/FindPivotIndexTest.java
@@ -0,0 +1,39 @@
+package io.github.bluething.leetcode.learn.arrayandstring;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/*
+* The problem https://leetcode.com/explore/learn/card/array-and-string/201/introduction-to-array/1144/
+* */
+public class FindPivotIndexTest {
+
+    @Test
+    public void successWhenInputIsRight() {
+        Assert.assertEquals(3, pivotIndex(new int[]{1,7,3,6,5,6}));
+    }
+
+    private int pivotIndex(int[] nums) {
+        int arrLength = nums.length;
+        if(arrLength == 0 || arrLength == 1 || arrLength == 2)
+            return -1;
+        int sumLeft = 0;
+        int sumRight = 0;
+        for (int i = 1; i < arrLength-2; i++) {
+            sumLeft=0;
+            sumRight=0;
+            for(int j = 0; j<i; j++) {
+                sumLeft += nums[j];
+            }
+            for (int k = i+1; k < arrLength; k++) {
+                sumRight += nums[k];
+            }
+            if(sumLeft == sumRight) {
+                return i;
+            }
+
+        }
+        return -1;
+    }
+
+}
