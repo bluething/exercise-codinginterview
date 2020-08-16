d5c50394b63660b74d5de7825c03e1416afda81f
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-08-16 16:38:39 +0700

    2nd solution
    Little improvement, I delete maxValue variable. We can get the value from the array if we know the max index.

--- leetcode/learn/src/test/java/io/github/bluething/leetcode/learn/arrayandstring/LargestNumberAtLeastTwiceofOthersTest.java
+++ leetcode/learn/src/test/java/io/github/bluething/leetcode/learn/arrayandstring/LargestNumberAtLeastTwiceofOthersTest.java
@@ -14,16 +14,14 @@ public class LargestNumberAtLeastTwiceofOthersTest {
     }
 
     private int dominantIndex(int[] nums) {
-        int maxValue = 0;
         int maxIndex = 0;
         for (int i=0; i < nums.length; i++) {
-            if(nums[i] >= maxValue) {
-                maxValue = nums[i];
+            if(nums[i] >= nums[maxIndex]) {
                 maxIndex = i;
             }
         }
         for (int i=0; i < nums.length; i++) {
-            if(i != maxIndex && maxValue < 2 * nums[i]) {
+            if(i != maxIndex && nums[maxIndex] < 2 * nums[i]) {
                 return -1;
             }
         }
