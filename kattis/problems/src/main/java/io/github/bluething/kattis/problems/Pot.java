37d8deb9bc1825db0a2472d182e3a531271dd6d9
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-09-09 11:41:00 +0700

    Pot 1st solution

---
+++ kattis/problems/src/main/java/io/github/bluething/kattis/problems/Pot.java
@@ -0,0 +1,24 @@
+package io.github.bluething.kattis.problems;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+* The problem https://open.kattis.com/problems/pot
+* */
+public class Pot {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int numberOfLine = Integer.valueOf(bufferedReader.readLine());
+        String input;
+        int originalTask = 0;
+        for(int i = 1; i <= numberOfLine; i++) {
+            input = bufferedReader.readLine();
+            int pow = Integer.parseInt(input.substring(input.length()-1));
+            int number = Integer.parseInt(input.substring(0, input.length()-1));
+            originalTask += Math.pow(number, pow);
+        }
+        System.out.println(originalTask);
+    }
+}
