49604be13445af3e9421aed81b524b08aa95305e
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-10-20 17:53:24 +0700

    Ransom Note 2nd solution

--- hackerrank/interviewpreparationkit/src/test/java/io/github/bluething/hackerrank/interviewpreparationkit/dictionariesandhashmaps/RansomNoteTest.java
+++ hackerrank/interviewpreparationkit/src/test/java/io/github/bluething/hackerrank/interviewpreparationkit/dictionariesandhashmaps/RansomNoteTest.java
@@ -8,24 +8,43 @@ import java.util.Map;
 
 /*
 * The problem https://www.hackerrank.com/challenges/ctci-ransom-note/problem
+* The idea is
+* 1. Don't forget about corner cases!
+* 2. Get word count of magazine array, save to map
+* 3. No need doing (2) for note array
+* 4. We only need to iterate note array. In each loop, we decrease word count of magazine map (if the word exist)
+* 5. If magazine map don't have a note word, result is 'No'
+* 6. If word count in magazine map < 0, result is 'No'
 * */
 public class RansomNoteTest {
 
     private String checkMagazine(String[] magazine, String[] note) {
+
+        String result = "Yes";
+
+        // corner case 1, note array is empty
+        if(note == null || note.length == 0) {
+            result = "No";
+        }
+
+        // corner case 2, note array is greater than the length of the magazine array
+        if(note.length > magazine.length) {
+            result = "No";
+        }
+
         Map<String, Integer> magazineWords = new HashMap<>();
         for (String word : magazine) {
             magazineWords.put(word, magazineWords.getOrDefault(word, 0) + 1);
         }
 
-        Map<String, Integer> noteWords = new HashMap<>();
         for (String word : note) {
-            noteWords.put(word, noteWords.getOrDefault(word, 0) + 1);
-        }
-
-        String result = "Yes";
-
-        for (String word : noteWords.keySet()) {
-            if (noteWords.get(word) != magazineWords.get(word)) {
+            if (magazineWords.get(word) != null) {
+                magazineWords.put(word, magazineWords.get(word) - 1);
+                if (magazineWords.get(word) < 0) {
+                    result = "No";
+                    break;
+                }
+            } else {
                 result = "No";
                 break;
             }
