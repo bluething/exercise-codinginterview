b8406505511b41a26c0e314c7bbdcfd1d1b9b085
Habib Ali Machpud <habib.gunadarma@gmail.com>
2021-01-18 14:33:11 +0700

    Refcator
    Take out inner condition

--- kattis/problems/src/main/java/io/github/bluething/kattis/problems/Cetvrta.java
+++ kattis/problems/src/main/java/io/github/bluething/kattis/problems/Cetvrta.java
@@ -25,25 +25,18 @@ public class Cetvrta {
 
         if (x1 == x2) {
             x4 = x3;
-            if (y3 == y1) {
-                y4 = y2;
-            } else {
-                y4 = y1;
-            }
         } else if (x1 == x3) {
             x4 = x2;
-            if (y2 == y1) {
-                y4 = y3;
-            } else {
-                y4 = y1;
-            }
         } else {
             x4 = x1;
-            if (y2 == y1) {
-                y4 = y3;
-            } else {
-                y4 = y2;
-            }
+        }
+
+        if (y1 == y2) {
+            y4 = y3;
+        } else if (y1 == y3) {
+            y4 = y2;
+        } else {
+            y4 = y1;
         }
 
         System.out.printf("%d %d", x4, y4);
