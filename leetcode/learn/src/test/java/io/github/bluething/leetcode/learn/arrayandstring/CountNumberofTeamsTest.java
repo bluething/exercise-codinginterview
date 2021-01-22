f28cf42b118f0a4ef2898e847831aacb2e3710bb
Habib Ali Machpud <habib.gunadarma@gmail.com>
2021-01-22 14:31:19 +0700

    Count Number of Teams 2nd solution
    Small optimization.
    Change from else if into one if.

--- leetcode/learn/src/test/java/io/github/bluething/leetcode/learn/arrayandstring/CountNumberofTeamsTest.java
+++ leetcode/learn/src/test/java/io/github/bluething/leetcode/learn/arrayandstring/CountNumberofTeamsTest.java
@@ -27,9 +27,8 @@ public class CountNumberofTeamsTest {
         for (int i = 0; i < rating.length; i++) {
             for (int j = i + 1; j < rating.length; j++) {
                 for (int k = j + 1; k < rating.length; k++) {
-                    if (rating[i] > rating[j] && rating[j] > rating[k]) {
-                        counter++;
-                    } else if (rating[i] < rating[j] && rating[j] < rating[k]) {
+                    if ((rating[i] > rating[j] && rating[j] > rating[k]) ||
+                            (rating[i] < rating[j] && rating[j] < rating[k])) {
                         counter++;
                     }
                 }
