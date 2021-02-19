c4fb353ffc226a89a16b79a98d681da42df8a296
Habib Ali Machpud <habib.gunadarma@gmail.com>
2021-02-19 17:49:49 +0700

    Minimum Remove to Make Valid Parentheses 2nd solution

--- leetcode/learn/src/test/java/io/github/bluething/leetcode/februaryleetCodingchallenge2021/MinimumRemovetoMakeValidParenthesesTest.java
+++ leetcode/learn/src/test/java/io/github/bluething/leetcode/februaryleetCodingchallenge2021/MinimumRemovetoMakeValidParenthesesTest.java
@@ -3,7 +3,6 @@ package io.github.bluething.leetcode.februaryleetCodingchallenge2021;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Calendar;
 import java.util.Stack;
 
 public class MinimumRemovetoMakeValidParenthesesTest {
@@ -29,26 +28,39 @@ public class MinimumRemovetoMakeValidParenthesesTest {
     }
 
     private String minRemoveToMakeValid(String s) {
-        StringBuilder validString = new StringBuilder("");
-        Stack<Character> pairParenthese = new Stack<>();
-        Character openParenthese = Character.valueOf('(');
+        Stack<Integer> removeIdxs = new Stack<>();
         char currentChar = (char) 0;
         for (int i = 0; i < s.length(); i++) {
             currentChar = s.charAt(i);
             if (currentChar == '(') {
-                pairParenthese.push(currentChar);
-            } else if (currentChar == ')' && !pairParenthese.isEmpty()) {
-                openParenthese = pairParenthese.pop();
-                if (openParenthese != null) {
-                    validString.append(openParenthese).append(currentChar);
+                removeIdxs.push(Integer.valueOf(i));
+            } else if (currentChar == ')') {
+                if (!removeIdxs.isEmpty() && s.charAt(removeIdxs.peek()) == '(') {
+                    removeIdxs.pop();
+                } else {
+                    removeIdxs.push(Integer.valueOf(i));
                 }
-            } else {
-                validString.append(currentChar);
             }
+        }
+
+        StringBuilder validString = new StringBuilder(s);
+        int removeIndex = 0;
+        while (!removeIdxs.isEmpty()) {
+            removeIndex = removeIdxs.pop().intValue();
+            validString.deleteCharAt(removeIndex);
         }
 
         return validString.toString();
     }
 
-    // 1st I think can use Stack, but I was wrong. I miss the detail in sample output
+    // 1st I think can use Stack to hold Parentheses pair
+    // I think it's better to hold the idx of unpair parentheses
+    // This index used at final loop to clean up string builder
+    // If '(' found, push it
+    // If ')' found, THIS IS TRICKY PART!
+    // Normally we pop the stack (because pair), but there are one more condition
+    // We need to see at peek stack if the value is ')'
+    // Special case is when we have "))", if we don't peek the stack first, we will lose removeIndex
+    // If stack is empty, push it
+    // The final loop is clear the stack, in each loop, delete char at appropriate index
 }
