b87741b978788c0d2575bc6d60684b4c83f9758b
Habib Ali Machpud <habib.gunadarma@gmail.com>
2021-03-01 21:10:57 +0700

    Maximum Frequency Stack 1st solution

--- leetcode/learn/src/test/java/io/github/bluething/leetcode/februaryleetCodingchallenge2021/MaximumFrequencyStackTest.java
+++ leetcode/learn/src/test/java/io/github/bluething/leetcode/februaryleetCodingchallenge2021/MaximumFrequencyStackTest.java
@@ -3,6 +3,8 @@ package io.github.bluething.leetcode.februaryleetCodingchallenge2021;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.*;
+
 public class MaximumFrequencyStackTest {
 
     @Test
@@ -67,16 +69,33 @@ public class MaximumFrequencyStackTest {
 
     class FreqStack {
 
-        public FreqStack() {
+        Map<Integer, Integer> freqMap;
+        List<Stack<Integer>> stacks;
 
+        public FreqStack() {
+            freqMap = new HashMap<>();
+            stacks = new ArrayList<>();
+            stacks.add(new Stack<>());
         }
 
         public void push(int x) {
-
+            int freq = freqMap.getOrDefault(Integer.valueOf(x), 0) + 1;
+            freqMap.put(Integer.valueOf(x), freq);
+            if (freq == stacks.size()) {
+                stacks.add(new Stack<>());
+            }
+            stacks.get(freq).add(Integer.valueOf(x));
         }
 
         public int pop() {
-            return 0;
+            Stack<Integer> top = stacks.get(stacks.size() - 1);
+            int x = top.pop();
+            if (top.size() == 0) {
+                stacks.remove(stacks.size() -  1);
+            }
+            freqMap.put(Integer.valueOf(x), freqMap.get(Integer.valueOf(x)) -  1);
+
+            return x;
         }
     }
 }
