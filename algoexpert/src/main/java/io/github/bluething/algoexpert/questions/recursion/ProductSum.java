8bddc8c6e7fcd26931dc13612fcc99924be67247
bluething <habib.gunadarma@gmail.com>
2021-07-28 09:17:01 +0700

    1st solution
    failed for some cases

--- algoexpert/src/main/java/io/github/bluething/algoexpert/questions/recursion/ProductSum.java
+++ algoexpert/src/main/java/io/github/bluething/algoexpert/questions/recursion/ProductSum.java
@@ -1,37 +1,50 @@
 package io.github.bluething.algoexpert.questions.recursion;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ProductSum {
 
     public static void main(String[] args) {
         List<Object> array = new ArrayList<>();
-        array.add(Integer.valueOf(5));
-        array.add(Integer.valueOf(2));
-        array.add(new Integer[]{7, -1});
-        array.add(Integer.valueOf(3));
-        array.add(new Object[]{Integer.valueOf(6), new Integer[]{13, 8}});
-        array.add(Integer.valueOf(4));
+        array.add(5);
+        array.add(2);
+        array.add(Arrays.asList(7, -1));
+        array.add(3);
+        array.add(Arrays.asList(6, Arrays.asList(-13, 8)));
+        array.add(4);
         System.out.println("case 01 " + productSum(array));
 
         array = new ArrayList<>();
-        array.add(Integer.valueOf(1));
-        array.add(Integer.valueOf(2));
-        array.add(new Integer[]{3});
-        array.add(Integer.valueOf(4));
-        array.add(Integer.valueOf(5));
+        array.add(1);
+        array.add(2);
+        array.add(Arrays.asList(3));
+        array.add(4);
+        array.add(5);
         System.out.println("case 02 " + productSum(array));
 
         array = new ArrayList<>();
-        array.add(new Integer[]{1, 2});
-        array.add(Integer.valueOf(3));
-        array.add(new Integer[]{4, 5});
+        array.add(Arrays.asList(1, 2));
+        array.add(3);
+        array.add(Arrays.asList(4, 5));
         System.out.println("case 03 " + productSum(array));
     }
 
     private static int productSum(List<Object> array) {
-        // Write your code here.
-        return -1;
+        return productSumRec(array, 1);
+    }
+
+    private static int productSumRec(List<Object> array, int multiplier) {
+        int sum = 0;
+        for (Object obj : array) {
+            if (obj instanceof List) {
+                List<Object> values = (List<Object>) obj;
+                sum += productSumRec(values, multiplier++);
+            } else {
+                sum += (int) obj;
+            }
+        }
+        return sum * multiplier;
     }
 }
