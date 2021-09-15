8fc59e2b1b73f5266a98aa051e4d296919ca6f99
bluething <habib.gunadarma@gmail.com>
2021-09-15 08:44:08 +0700

    Some description

--- algoexpert/src/main/java/io/github/bluething/algoexpert/questions/sorting/BubbleSort.java
+++ algoexpert/src/main/java/io/github/bluething/algoexpert/questions/sorting/BubbleSort.java
@@ -20,4 +20,6 @@ public class BubbleSort {
         }
         return array;
     }
+
+    // Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order.
 }
