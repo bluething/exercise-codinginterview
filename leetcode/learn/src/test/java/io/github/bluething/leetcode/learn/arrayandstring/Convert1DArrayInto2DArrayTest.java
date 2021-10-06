cd5b5333c64163f14d1e06ccc68c940013e9ec4a
bluething <habib.gunadarma@gmail.com>
2021-10-06 11:04:38 +0700

    Convert 1D Array Into 2D Array 1st solution
    Not optimize

---
+++ leetcode/learn/src/test/java/io/github/bluething/leetcode/learn/arrayandstring/Convert1DArrayInto2DArrayTest.java
@@ -0,0 +1,34 @@
+package io.github.bluething.leetcode.learn.arrayandstring;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Convert1DArrayInto2DArrayTest {
+    @Test
+    public void case01() {
+        Assert.assertArrayEquals(new int[][] {{1, 2, 3}}, construct2DArray(new int[]{1, 2, 3}, 1, 3));
+    }
+
+    @Test
+    public void case02() {
+        Assert.assertArrayEquals(new int[][] {{1, 2}, {3, 4}}, construct2DArray(new int[]{1, 2, 3, 4}, 2, 2));
+    }
+
+    @Test
+    public void case03() {
+        Assert.assertArrayEquals(new int[][]{}, construct2DArray(new int[]{1, 2}, 1, 1));
+    }
+
+    private int[][] construct2DArray(int[] original, int m, int n) {
+        if (original.length != m * n) {
+            return new int[][]{};
+        }
+
+        int[][] result = new int[m][n];
+        for (int i = 0; i < m * n; i++) {
+            result[i/n][i%n] = original[i];
+        }
+
+        return result;
+    }
+}
