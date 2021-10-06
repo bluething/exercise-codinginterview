6a1073d715555b6d5858a5aaa034be24d2749eb2
bluething <habib.gunadarma@gmail.com>
2021-10-06 11:09:52 +0700

    Convert 1D Array Into 2D Array 3rd solution
    The optimization when we calculate which index to get

--- leetcode/learn/src/test/java/io/github/bluething/leetcode/learn/arrayandstring/Convert1DArrayInto2DArrayTest.java
+++ leetcode/learn/src/test/java/io/github/bluething/leetcode/learn/arrayandstring/Convert1DArrayInto2DArrayTest.java
@@ -25,9 +25,10 @@ public class Convert1DArrayInto2DArrayTest {
         }
 
         int[][] result = new int[m][n];
+        int idx = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                result[i][j] = original[n*i+j];
+                result[i][j] = original[idx++];
             }
         }
 
