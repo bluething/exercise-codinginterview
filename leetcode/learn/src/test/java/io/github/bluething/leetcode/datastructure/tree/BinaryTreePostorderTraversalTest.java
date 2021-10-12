52edb662ac55c7aadfeb3d0a86fd47ca7bad8794
bluething <habib.gunadarma@gmail.com>
2021-10-12 22:09:18 +0700

    Binary Tree Postorder Traversal 2nd solution
    Iteration

--- leetcode/learn/src/test/java/io/github/bluething/leetcode/datastructure/tree/BinaryTreePostorderTraversalTest.java
+++ leetcode/learn/src/test/java/io/github/bluething/leetcode/datastructure/tree/BinaryTreePostorderTraversalTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 public class BinaryTreePostorderTraversalTest {
 
@@ -19,6 +20,18 @@ public class BinaryTreePostorderTraversalTest {
 
         Assert.assertEquals(expectation, postorderTraversal(one));
     }
+
+    @Test
+    public void case02() {
+        TreeNode three = new TreeNode(3);
+        TreeNode two = new TreeNode(2, three, null);
+        TreeNode one = new TreeNode(1, null, two);
+
+        List<Integer> expectation = Arrays.asList(3, 2, 1);
+
+        Assert.assertEquals(expectation, postorderTraversalIterative(one));
+    }
+
     private List<Integer> postorderTraversal(TreeNode root) {
         if (root == null) {
             return new ArrayList<>();
@@ -26,6 +39,38 @@ public class BinaryTreePostorderTraversalTest {
 
         List<Integer> values = new ArrayList<>();
         root.postorderTraversalRecursion(root, values);
+
+        return values;
+    }
+
+    private List<Integer> postorderTraversalIterative(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+
+        List<Integer> values = new ArrayList<>();
+        Stack<TreeNode> nodes = new Stack<>();
+        TreeNode current = root;
+        TreeNode prev = null;
+
+        while (!nodes.empty() || current != null) {
+            // push leftmost nodes
+            while (current != null) {
+                nodes.push(current);
+                current = current.left;
+            }
+
+            // if the last node don't have right child
+            // or
+            // the right child is the prev (we already traversed all the nodes of stack.peek())
+            if (nodes.peek().right == null || nodes.peek().right == prev) {
+                prev = nodes.pop();
+                values.add(prev.value);
+            } else {
+                // visit the right child
+                current = nodes.peek().right;
+            }
+        }
 
         return values;
     }
