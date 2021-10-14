0cfcbf0996ba64f5c42809b543f5748c1d12d0a0
bluething <habib.gunadarma@gmail.com>
2021-10-14 21:51:10 +0700

    Invert Binary Tree 2nd solution
    Using iteration

--- leetcode/learn/src/test/java/io/github/bluething/leetcode/datastructure/tree/InvertBinaryTreeTest.java
+++ leetcode/learn/src/test/java/io/github/bluething/leetcode/datastructure/tree/InvertBinaryTreeTest.java
@@ -3,6 +3,8 @@ package io.github.bluething.leetcode.datastructure.tree;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Stack;
+
 public class InvertBinaryTreeTest {
     @Test
     public void case01() {
@@ -26,6 +28,28 @@ public class InvertBinaryTreeTest {
 
     }
 
+    @Test
+    public void case02() {
+        TreeNode inputNine = new TreeNode(9);
+        TreeNode inputSix = new TreeNode(6);
+        TreeNode inputSeven = new TreeNode(7, inputSix, inputNine);
+        TreeNode inputOne = new TreeNode(1);
+        TreeNode inputThree = new TreeNode(3);
+        TreeNode inputTwo = new TreeNode(2, inputOne, inputThree);
+        TreeNode inputFour = new TreeNode(4, inputTwo, inputSeven);
+
+        TreeNode outputNine = new TreeNode(9);
+        TreeNode outputSix = new TreeNode(6);
+        TreeNode outputSeven = new TreeNode(7, outputNine, outputSix);
+        TreeNode outputOne = new TreeNode(1);
+        TreeNode outputThree = new TreeNode(3);
+        TreeNode outputTwo = new TreeNode(2, outputOne, outputThree);
+        TreeNode outputFour = new TreeNode(4, outputSeven, outputTwo);
+
+        Assert.assertEquals(outputFour.left.value, invertTreeIterative(inputFour).left.value);
+
+    }
+
     private TreeNode invertTree(TreeNode root) {
         if (root == null) {
             return null;
@@ -37,6 +61,31 @@ public class InvertBinaryTreeTest {
 
         invertTree(root.left);
         invertTree(root.right);
+
+        return root;
+    }
+
+    // watch the stack push order
+    private TreeNode invertTreeIterative(TreeNode root) {
+        if (root == null) {
+            return  null;
+        }
+
+        Stack<TreeNode> nodes = new Stack<>();
+        nodes.push(root);
+
+        TreeNode temp = null;
+        while (!nodes.empty()) {
+            temp = nodes.pop();
+            if (temp != null) {
+                nodes.push(temp.right);
+                nodes.push(temp.left);
+
+                temp = root.left;
+                root.left = root.right;
+                root.right = temp;
+            }
+        }
 
         return root;
     }
