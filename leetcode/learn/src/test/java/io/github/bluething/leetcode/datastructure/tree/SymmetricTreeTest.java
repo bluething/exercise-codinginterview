3ce67f8125c6857f279159374750669320dba1ce
bluething <habib.gunadarma@gmail.com>
2021-10-14 15:03:20 +0700

    Symmetric Tree 2nd solution

--- leetcode/learn/src/test/java/io/github/bluething/leetcode/datastructure/tree/SymmetricTreeTest.java
+++ leetcode/learn/src/test/java/io/github/bluething/leetcode/datastructure/tree/SymmetricTreeTest.java
@@ -3,6 +3,8 @@ package io.github.bluething.leetcode.datastructure.tree;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Stack;
+
 public class SymmetricTreeTest {
     @Test
     public void case01() {
@@ -28,6 +30,30 @@ public class SymmetricTreeTest {
         Assert.assertFalse(isSymmetric(one));
     }
 
+    @Test
+    public void case03() {
+        TreeNode four1 = new TreeNode(4);
+        TreeNode three1 = new TreeNode(3);
+        TreeNode two1 = new TreeNode(2, three1, four1);
+        TreeNode four2 = new TreeNode(4);
+        TreeNode three2 = new TreeNode(3);
+        TreeNode two2 = new TreeNode(2, three2, four2);
+        TreeNode one = new TreeNode(1, two1, two2);
+
+        Assert.assertTrue(isSymmetricIterative(one));
+    }
+
+    @Test
+    public void case04() {
+        TreeNode three1 = new TreeNode(3);
+        TreeNode two1 = new TreeNode(2, null, three1);
+        TreeNode three2 = new TreeNode(3);
+        TreeNode two2 = new TreeNode(2, null, three2);
+        TreeNode one = new TreeNode(1, two1, two2);
+
+        Assert.assertFalse(isSymmetricIterative(one));
+    }
+
     private boolean isSymmetric(TreeNode root) {
         return traverse(root.left, root.right);
     }
@@ -48,6 +74,45 @@ public class SymmetricTreeTest {
         } else {
             return false;
         }
+    }
+
+    // The idea is using stack to save left and right side
+    // -- be careful about the order we push --
+    // in each iteration we pop two topmost nodes (the mirror nodes)
+    // return false if one of the side have null or different value
+    // else (both have same value)
+    // compare left and right (push into the stack)
+    // push left.left and right.right, left.right and right.left
+    private boolean isSymmetricIterative(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        Stack<TreeNode> nodes = new Stack<>();
+        nodes.push(root.left);
+        nodes.push(root.right);
+
+        TreeNode tempLeft = null;
+        TreeNode tempRight = null;
+        while (!nodes.empty()) {
+            tempRight = nodes.pop();
+            tempLeft = nodes.pop();
+
+            if (tempLeft == null && tempRight == null) {
+                continue;
+            }
+
+            if (tempLeft == null || tempRight == null || (tempLeft.value != tempRight.value)) {
+                return false;
+            }
+
+            nodes.push(tempLeft.left);
+            nodes.push(tempRight.right);
+            nodes.push(tempLeft.right);
+            nodes.push(tempRight.left);
+        }
+
+        return true;
     }
 
     class TreeNode {
