b06d795aabfaa2f9e7debbb7f5dabb72b711e9ae
bluething <habib.gunadarma@gmail.com>
2021-10-14 21:42:06 +0700

    Invert Binary Tree 1st solution
    Using recursion

---
+++ leetcode/learn/src/test/java/io/github/bluething/leetcode/datastructure/tree/InvertBinaryTreeTest.java
@@ -0,0 +1,75 @@
+package io.github.bluething.leetcode.datastructure.tree;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InvertBinaryTreeTest {
+    @Test
+    public void case01() {
+        TreeNode inputNine = new TreeNode(9);
+        TreeNode inputSix = new TreeNode(6);
+        TreeNode inputSeven = new TreeNode(7, inputSix, inputNine);
+        TreeNode inputOne = new TreeNode(1);
+        TreeNode inputThree = new TreeNode(3);
+        TreeNode inputTwo = new TreeNode(2, inputOne, inputThree);
+        TreeNode inputFour = new TreeNode(4, inputTwo, inputSeven);
+
+        TreeNode outputNine = new TreeNode(9);
+        TreeNode outputSix = new TreeNode(6);
+        TreeNode outputSeven = new TreeNode(7, outputNine, outputSix);
+        TreeNode outputOne = new TreeNode(1);
+        TreeNode outputThree = new TreeNode(3);
+        TreeNode outputTwo = new TreeNode(2, outputOne, outputThree);
+        TreeNode outputFour = new TreeNode(4, outputSeven, outputTwo);
+
+        Assert.assertEquals(outputFour.left.value, invertTree(inputFour).left.value);
+
+    }
+
+    private TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+
+    class TreeNode {
+        int value;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+
+        }
+
+        TreeNode(int value) {
+            this(value, null, null);
+        }
+
+        TreeNode(int value, TreeNode left, TreeNode right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+
+        public TreeNode getRight() {
+            return right;
+        }
+
+        public TreeNode getLeft() {
+            return left;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+}
