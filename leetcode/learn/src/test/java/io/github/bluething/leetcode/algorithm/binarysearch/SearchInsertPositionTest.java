a2d43a48e2b4cf80b80fac598939381d1b0570fb
bluething <habib.gunadarma@gmail.com>
2021-10-17 11:33:09 +0700

    Search Insert Position 2nd solution
    Using recursion

--- leetcode/learn/src/test/java/io/github/bluething/leetcode/algorithm/binarysearch/SearchInsertPositionTest.java
+++ leetcode/learn/src/test/java/io/github/bluething/leetcode/algorithm/binarysearch/SearchInsertPositionTest.java
@@ -32,6 +32,31 @@ public class SearchInsertPositionTest {
         Assert.assertEquals(0, searchInsert(new int[]{1}, 0));
     }
 
+    @Test
+    public void case06() {
+        Assert.assertEquals(2, searchInsertRecursion(new int[]{1,3,5,6}, 5));
+    }
+
+    @Test
+    public void case07() {
+        Assert.assertEquals(1, searchInsertRecursion(new int[]{1,3,5,6}, 2));
+    }
+
+    @Test
+    public void case08() {
+        Assert.assertEquals(4, searchInsertRecursion(new int[]{1,3,5,6}, 7));
+    }
+
+    @Test
+    public void case09() {
+        Assert.assertEquals(0, searchInsertRecursion(new int[]{1,3,5,6}, 0));
+    }
+
+    @Test
+    public void case10() {
+        Assert.assertEquals(0, searchInsertRecursion(new int[]{1}, 0));
+    }
+
     // same with finding target in nums
     // but now we need to return expected idx when target not found in nums
     // if the target found, return the idx
@@ -56,5 +81,26 @@ public class SearchInsertPositionTest {
         }
 
         return idx;
+    }
+
+    private int searchInsertRecursion(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int mid = low + ((high - low) / 2);
+        return helper(nums, target, low, mid, high);
+    }
+    private int helper(int[] nums, int target, int low, int mid, int high) {
+        if (low > high) {
+            return low;
+        }
+
+        mid = low + ((high - low) / 2);
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            return helper(nums, target, low, mid, mid - 1);
+        } else {
+            return helper(nums, target, mid + 1, mid, high);
+        }
     }
 }
