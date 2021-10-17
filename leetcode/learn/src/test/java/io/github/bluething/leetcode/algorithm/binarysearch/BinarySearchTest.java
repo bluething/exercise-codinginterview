09884353b959d666b4873678e7f28051f90e42f6
bluething <habib.gunadarma@gmail.com>
2021-10-17 10:52:03 +0700

    Binary search 2nd solution
    Using recursion

--- leetcode/learn/src/test/java/io/github/bluething/leetcode/algorithm/binarysearch/BinarySearchTest.java
+++ leetcode/learn/src/test/java/io/github/bluething/leetcode/algorithm/binarysearch/BinarySearchTest.java
@@ -9,8 +9,24 @@ public class BinarySearchTest {
         Assert.assertEquals(4, search(new int[]{-1,0,3,5,9,12}, 9));
     }
 
+    @Test
     public void case02() {
         Assert.assertEquals(-1, search(new int[]{-1,0,3,5,9,12}, 2));
+    }
+
+    @Test
+    public void case03() {
+        Assert.assertEquals(4, searchRecursion(new int[]{-1,0,3,5,9,12}, 9));
+    }
+
+    @Test
+    public void case04() {
+        Assert.assertEquals(-1, searchRecursion(new int[]{-1,0,3,5,9,12}, 2));
+    }
+
+    @Test
+    public void case05() {
+        Assert.assertEquals(4, searchRecursion(new int[]{-1,0,3,5,9,12}, 9));
     }
 
     private int search(int[] nums, int target) {
@@ -31,5 +47,26 @@ public class BinarySearchTest {
         }
 
         return idx;
+    }
+
+    private int searchRecursion(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int mid = low + ((high - low) / 2);
+        return helper(nums, target, low, mid, high);
+    }
+    private int helper(int[] nums, int target, int low, int mid, int high) {
+        if (low > high) {
+            return -1;
+        }
+
+        mid = low + ((high - low) / 2);
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            return helper(nums, target, low, mid, mid - 1);
+        } else {
+            return helper(nums, target, mid + 1, mid, high);
+        }
     }
 }
