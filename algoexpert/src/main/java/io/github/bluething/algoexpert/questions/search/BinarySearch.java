ed5fb97fec0243eadd4ca49fe2346e69e9e2cdfc
bluething <habib.gunadarma@gmail.com>
2021-11-01 22:23:23 +0700

    Binary search 2nd solution
    Iteration

--- algoexpert/src/main/java/io/github/bluething/algoexpert/questions/search/BinarySearch.java
+++ algoexpert/src/main/java/io/github/bluething/algoexpert/questions/search/BinarySearch.java
@@ -4,6 +4,7 @@ public class BinarySearch {
 
     public static void main(String[] args) {
         System.out.println(binarySearch(new int[]{0, 1, 21, 33, 45, 45, 61, 71, 72, 73}, 33));
+        System.out.println(binarySearchIteration(new int[]{0, 1, 21, 33, 45, 45, 61, 71, 72, 73}, 33));
     }
 
     private static int binarySearch(int[] array, int target) {
@@ -27,5 +28,28 @@ public class BinarySearch {
         } else {
             return helper(array, mid+1, high, mid, target);
         }
+    }
+
+    private static int binarySearchIteration(int[] array, int target) {
+        int low = 0;
+        int high = array.length - 1;
+
+        return helperIteration(array, low, high, target);
+    }
+
+    private static int helperIteration(int[] array, int low, int high, int target) {
+        int mid = 0;
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (array[mid] == target) {
+                return mid;
+            } else if (array[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return -1;
     }
 }
