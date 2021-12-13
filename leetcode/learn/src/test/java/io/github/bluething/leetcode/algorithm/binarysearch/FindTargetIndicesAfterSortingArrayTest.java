0c023a247d9d6a078c4809520b14ca1597075a37
bluething <habib.gunadarma@gmail.com>
2021-12-13 09:31:35 +0700

    Find Target Indices After Sorting Array 2nd solution
    Without sorting

--- leetcode/learn/src/test/java/io/github/bluething/leetcode/algorithm/binarysearch/FindTargetIndicesAfterSortingArrayTest.java
+++ leetcode/learn/src/test/java/io/github/bluething/leetcode/algorithm/binarysearch/FindTargetIndicesAfterSortingArrayTest.java
@@ -31,6 +31,24 @@ public class FindTargetIndicesAfterSortingArrayTest {
         Assert.assertEquals(expected, targetIndices(new int[]{1,2,5,2,3}, 4));
     }
 
+    @Test
+    public void case04() {
+        List<Integer> expected = Arrays.asList(1, 2);
+        Assert.assertEquals(expected, targetIndices2(new int[]{1,2,5,2,3}, 2));
+    }
+
+    @Test
+    public void case05() {
+        List<Integer> expected = Arrays.asList(3);
+        Assert.assertEquals(expected, targetIndices2(new int[]{1,2,5,2,3}, 3));
+    }
+
+    @Test
+    public void case06() {
+        List<Integer> expected = new ArrayList<>();
+        Assert.assertEquals(expected, targetIndices2(new int[]{1,2,5,2,3}, 4));
+    }
+
     private List<Integer> targetIndices(int[] nums, int target) {
         Arrays.sort(nums);
 
@@ -39,6 +57,31 @@ public class FindTargetIndicesAfterSortingArrayTest {
             if (nums[i] == target) {
                 idx.add(i);
             }
+        }
+
+        return idx;
+    }
+
+    // more optimize solution
+    // We count how many element less than target (lowerIdx) and how many element equal to target (targetIdx)
+    // Loop as much targetIdx, in each iteration we increment lowerIdx
+    private List<Integer> targetIndices2(int[] nums, int target) {
+        int lowerIdx = 0;
+        int targetIdx = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < target) {
+                lowerIdx++;
+            }
+            if (nums[i] == target) {
+                targetIdx++;
+            }
+        }
+
+        Arrays.sort(nums);
+
+        List<Integer> idx = new ArrayList<>();
+        for (int i = 0; i < targetIdx; i++) {
+            idx.add(lowerIdx++);
         }
 
         return idx;
