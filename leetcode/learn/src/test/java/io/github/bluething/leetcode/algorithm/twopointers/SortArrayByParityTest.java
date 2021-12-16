32764b216f4390c2a0b2c95bfd8fe928f05e66b2
bluething <habib.gunadarma@gmail.com>
2021-12-16 11:18:15 +0700

    Sort Array By Parity 2nd solution

--- leetcode/learn/src/test/java/io/github/bluething/leetcode/algorithm/twopointers/SortArrayByParityTest.java
+++ leetcode/learn/src/test/java/io/github/bluething/leetcode/algorithm/twopointers/SortArrayByParityTest.java
@@ -9,6 +9,11 @@ public class SortArrayByParityTest {
         int[] actual = sortArrayByParity(new int[]{3,1,2,4});
     }
 
+    @Test
+    public void case02() {
+        int[] actual = sortArrayByParity2(new int[]{3,1,2,4});
+    }
+
     private int[] sortArrayByParity(int[] nums) {
         int insertIdx = 0;
         int tmp = 0;
@@ -20,6 +25,31 @@ public class SortArrayByParityTest {
                     nums[i] = tmp;
                 }
                 insertIdx++;
+            }
+        }
+
+        return nums;
+    }
+
+    private int[] sortArrayByParity2(int[] nums) {
+        int i = 0;
+        int j = nums.length - 1;
+        int tmp = 0;
+        while (i < j) {
+            // nums[i] must even when i < j
+            if (nums[i] % 2 > nums[j] % 2) {
+                tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+            }
+
+            // found even, move to the right
+            if (nums[i] % 2 == 0) {
+                i++;
+            }
+            // found odd, move to the left
+            if (nums[j] % 2 == 1) {
+                j--;
             }
         }
 
