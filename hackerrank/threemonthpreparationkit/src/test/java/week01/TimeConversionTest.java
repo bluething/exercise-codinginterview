3f7dec76f3667e099b08e7d29040e59a0f9c333e
bluething <habib.gunadarma@gmail.com>
2021-12-28 10:53:35 +0700

    Time Conversion 1st solution
    The challenge is how to convert the hour part. Use modulo oeprator.

---
+++ hackerrank/threemonthpreparationkit/src/test/java/week01/TimeConversionTest.java
@@ -0,0 +1,37 @@
+package week01;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TimeConversionTest {
+
+    @Test
+    public void case01() {
+        Assertions.assertEquals("19:05:45", timeConversion("07:05:45PM"));
+    }
+
+    @Test
+    public void case02() {
+        Assertions.assertEquals("00:00:00", timeConversion("12:00:00AM"));
+    }
+
+    @Test
+    public void case03() {
+        Assertions.assertEquals("04:59:59", timeConversion("04:59:59AM"));
+    }
+
+    private String timeConversion(String s) {
+        int hour = Integer.valueOf(s.substring(0, 2));
+        int minute = Integer.valueOf(s.substring(3, 5));
+        int second = Integer.valueOf(s.substring(6, 8));
+        boolean isAM = s.substring(8).equals("AM") ?  true : false;
+
+        if (isAM) {
+            hour = hour % 12;
+        } else {
+            hour = (hour % 12) + 12;
+        }
+
+        return String.format("%02d:%02d:%02d", hour, minute, second);
+    }
+}
