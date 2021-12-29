18512be294df5b10d7b75cf1cc46be800863f581
bluething <habib.gunadarma@gmail.com>
2021-12-29 18:44:21 +0700

    Divisible Sum Pairs 2nd solution
    Using array as a counter

--- hackerrank/threemonthpreparationkit/src/test/java/week01/DivisibleSumPairsTest.java
+++ hackerrank/threemonthpreparationkit/src/test/java/week01/DivisibleSumPairsTest.java
@@ -14,6 +14,11 @@ public class DivisibleSumPairsTest {
         Assertions.assertEquals(5, divisibleSumPairs(6, 3, Arrays.asList(1, 3, 2, 6, 1, 2)));
     }
 
+    @Test
+    public void case02() {
+        Assertions.assertEquals(5, divisibleSumPairs2(6, 3, Arrays.asList(1, 3, 2, 6, 1, 2)));
+    }
+
     private int divisibleSumPairs(int n, int k, List<Integer> ar) {
         int numOfPair = 0;
         for (int i = 0; i < ar.size() - 1; i++) {
@@ -22,6 +27,28 @@ public class DivisibleSumPairsTest {
                     numOfPair++;
                 }
             }
+        }
+
+        return numOfPair;
+    }
+
+    // (a + b) % K = 0 (what we have to find)
+    // Simplifying: => a%K + b%K = 0
+    //  => a%K + b%K = K%K
+    //  => b%K = K%K - a%K
+    //  => b%K = (K - a%K) % K. {Range of a%K => [0,K-1]}
+    // The idea is using an array of max size of n as a counter
+    // We count how many times ar[i]%k encountered
+    // For each element visited increment numOfPair by the value of an array at index (k - rem) % k
+    // (k - rem) % k is the pair of ar.get(i) when (a + b) % K = 0 meet
+    private int divisibleSumPairs2(int n, int k, List<Integer> ar) {
+        int numOfPair = 0;
+        int[] counters = new int[101];
+        for (int i = 0; i < ar.size(); i++) {
+            int rem = ar.get(i) % k;
+            int key = (k - rem) % k;
+            numOfPair += counters[key];
+            counters[rem]++;
         }
 
         return numOfPair;
