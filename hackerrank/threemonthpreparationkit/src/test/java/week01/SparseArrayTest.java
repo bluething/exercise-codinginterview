fcd3a39414b5c1f74c0ac1584e5ac7a646356bfb
bluething <habib.gunadarma@gmail.com>
2021-12-30 10:04:46 +0700

    Sparse Array 2nd solution
    Using map as a frequency counter

--- hackerrank/threemonthpreparationkit/src/test/java/week01/SparseArrayTest.java
+++ hackerrank/threemonthpreparationkit/src/test/java/week01/SparseArrayTest.java
@@ -3,9 +3,9 @@ package week01;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 // There is a collection of input strings and a collection of query strings.
 // For each query string, determine how many times it occurs in the list of input strings.
@@ -35,6 +35,25 @@ public class SparseArrayTest {
                         Arrays.asList("abcde", "sdaklfj", "asdjf", "na", "basdn")));
     }
 
+    @Test
+    public void case04() {
+        Assertions.assertEquals(Arrays.asList(2, 1, 0),
+                matchingStrings2(Arrays.asList("aba", "baba", "aba", "xzxb"), Arrays.asList("aba", "xzxb", "ab")));
+    }
+
+    @Test
+    public void case05() {
+        Assertions.assertEquals(Arrays.asList(1, 0, 1),
+                matchingStrings2(Arrays.asList("def", "de", "fgh"), Arrays.asList("de", "lmn", "fgh")));
+    }
+
+    @Test
+    public void case06() {
+        Assertions.assertEquals(Arrays.asList(1, 3, 4, 3, 2),
+                matchingStrings2(Arrays.asList("abcde", "sdaklfj", "asdjf", "na", "basdn", "sdaklfj", "asdjf", "na", "asdjf", "na", "basdn", "sdaklfj", "asdjf"),
+                        Arrays.asList("abcde", "sdaklfj", "asdjf", "na", "basdn")));
+    }
+
     private List<Integer> matchingStrings(List<String> strings, List<String> queries) {
         List<Integer> counters = new ArrayList<>();
         for (int i = 0; i < queries.size(); i++) {
@@ -49,4 +68,18 @@ public class SparseArrayTest {
 
         return counters;
     }
+    private List<Integer> matchingStrings2(List<String> strings, List<String> queries) {
+        Map<String, Integer> stringFreq = new HashMap<>();
+        for (int i = 0; i < strings.size(); i++) {
+            stringFreq.put(strings.get(i), stringFreq.getOrDefault(strings.get(i), 0) + 1);
+        }
+
+        List<Integer> counters = new ArrayList<>();
+        for (int i = 0; i < queries.size(); i++) {
+            counters.add(stringFreq.getOrDefault(queries.get(i), 0));
+        }
+
+        return counters;
+    }
+
 }
