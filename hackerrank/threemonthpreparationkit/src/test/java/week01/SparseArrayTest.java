62d5595c79599bf81f6dfa95cc7e02580a9e1cbe
bluething <habib.gunadarma@gmail.com>
2021-12-30 09:52:10 +0700

    Sparse Array 1st solution

---
+++ hackerrank/threemonthpreparationkit/src/test/java/week01/SparseArrayTest.java
@@ -0,0 +1,52 @@
+package week01;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// There is a collection of input strings and a collection of query strings.
+// For each query string, determine how many times it occurs in the list of input strings.
+// Return an array of the results.
+// strings = ["aba", "baba", "aba", "xzxb"]
+// queries = ["aba", "xzxb", "ab"]
+// There are 2 instances of "ab", 1 of "xzxb" and 0 of "ab".
+// For each query, add an element to the return array, results = [2, 1, 0]
+public class SparseArrayTest {
+
+    @Test
+    public void case01() {
+        Assertions.assertEquals(Arrays.asList(2, 1, 0),
+                matchingStrings(Arrays.asList("aba", "baba", "aba", "xzxb"), Arrays.asList("aba", "xzxb", "ab")));
+    }
+
+    @Test
+    public void case02() {
+        Assertions.assertEquals(Arrays.asList(1, 0, 1),
+                matchingStrings(Arrays.asList("def", "de", "fgh"), Arrays.asList("de", "lmn", "fgh")));
+    }
+
+    @Test
+    public void case03() {
+        Assertions.assertEquals(Arrays.asList(1, 3, 4, 3, 2),
+                matchingStrings(Arrays.asList("abcde", "sdaklfj", "asdjf", "na", "basdn", "sdaklfj", "asdjf", "na", "asdjf", "na", "basdn", "sdaklfj", "asdjf"),
+                        Arrays.asList("abcde", "sdaklfj", "asdjf", "na", "basdn")));
+    }
+
+    private List<Integer> matchingStrings(List<String> strings, List<String> queries) {
+        List<Integer> counters = new ArrayList<>();
+        for (int i = 0; i < queries.size(); i++) {
+            int counter = 0;
+            for (int j = 0; j < strings.size(); j++) {
+                if (strings.get(j).equals(queries.get(i))) {
+                    counter++;
+                }
+            }
+            counters.add(counter);
+        }
+
+        return counters;
+    }
+}
