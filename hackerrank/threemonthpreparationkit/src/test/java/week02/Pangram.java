25f4da7649aa62c22502c8666d983c0c96c4183e
bluething <habib.gunadarma@gmail.com>
2022-01-03 10:40:34 +0700

    Pangrams 2nd solution

--- hackerrank/threemonthpreparationkit/src/test/java/week02/Pangram.java
+++ hackerrank/threemonthpreparationkit/src/test/java/week02/Pangram.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 // A pangram is a string that contains every letter of the alphabet.
 // Given a sentence determine whether it is a pangram in the English alphabet. Ignore case. Return either pangram or not pangram as appropriate.
@@ -18,6 +20,16 @@ public class Pangram {
     @Test
     public void case02() {
         Assertions.assertEquals("pangram", pangrams("We promptly judged antique ivory buckles for the next prize"));
+    }
+
+    @Test
+    public void case03() {
+        Assertions.assertEquals("not pangram", pangrams2("We promptly judged antique ivory buckles for the prize"));
+    }
+
+    @Test
+    public void case04() {
+        Assertions.assertEquals("pangram", pangrams2("We promptly judged antique ivory buckles for the next prize"));
     }
 
     private String pangrams(String s) {
@@ -38,6 +50,26 @@ public class Pangram {
                 break;
             }
         }
+        return result;
+    }
+
+    // No need to count the frequency
+    // Just add all the chars in the string to set then check the size
+    // If the size is 26 (size of alphabet) then the string is pangram
+    private String pangrams2(String s) {
+        String result = "not pangram";
+
+        Set<Character> countUnique = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ') {
+                countUnique.add(Character.toLowerCase(s.charAt(i)));
+            }
+        }
+
+        if (countUnique.size() == 26) {
+            result = "pangram";
+        }
+
         return result;
     }
 }
