6ca86ba7f8f236e79e56745b7ad409676be4405f
bluething <habib.gunadarma@gmail.com>
2022-01-03 21:36:00 +0700

    Reshape the matrix 1st solution

---
+++ forfun/algorithmanddatastructure/src/test/java/io/github/bluething/interview/forfun/array/ReshapeTheMatrixTest.java
@@ -0,0 +1,56 @@
+package io.github.bluething.interview.forfun.array;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+// From two-dimensional array.
+// Transpose the array
+// Flatten the transposed array
+
+public class ReshapeTheMatrixTest {
+
+    @Test
+    public void case01() {
+        Assertions.assertArrayEquals(new int[]{1, 3, 7, 2, 4, 8, 3, 5, 9}, matrixReshape(new int[][]{{1, 2, 3}, {3, 4, 5}, {7, 8, 9}}));
+    }
+
+    @Test
+    public void case02() {
+        Assertions.assertArrayEquals(new int[][]{{1, 3, 7}, {2, 4, 8}, {3, 5, 9}}, transpose(new int[][]{{1, 2, 3}, {3, 4, 5}, {7, 8, 9}}));
+    }
+
+    @Test
+    public void case03() {
+        Assertions.assertArrayEquals(new int[]{1, 3, 7, 2, 4, 8, 3, 5, 9}, from2Dto1D(new int[][]{{1, 3, 7}, {2, 4, 8}, {3, 5, 9}}));
+    }
+
+    private int[] matrixReshape(int[][] matrix) {
+        return from2Dto1D(transpose(matrix));
+    }
+
+    private int[][] transpose(int[][] matrix) {
+        int temp;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                if (i != j) {
+                    temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                }
+            }
+        }
+        return matrix;
+    }
+
+    private int[] from2Dto1D(int[][] matrix) {
+        int column = matrix[0].length;
+        int numOfElement = matrix.length * column;
+        int[] oneD = new int[numOfElement];
+        for (int i = 0; i < numOfElement; i++) {
+            oneD[i] = matrix[i / column][i % column];
+        }
+
+        return oneD;
+    }
+
+}
