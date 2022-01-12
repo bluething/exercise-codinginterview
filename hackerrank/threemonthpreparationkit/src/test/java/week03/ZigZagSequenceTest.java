1d2ab34b1038e479eaa2e6d9f61b18cc277c2998
bluething <habib.gunadarma@gmail.com>
2022-01-12 14:28:20 +0700

    Zig Zag Sequence 1st solution

--- hackerrank/threemonthpreparationkit/src/test/java/week03/ZigZagSequenceTest.java
+++ hackerrank/threemonthpreparationkit/src/test/java/week03/ZigZagSequenceTest.java
@@ -10,24 +10,31 @@ public class ZigZagSequenceTest {
     public void case01() {
         int[] input = new int[]{2, 3, 5, 1, 4};
         findZigZagSequence(input, 5);
-        Assertions.assertArrayEquals(new int[]{1, 4, 5, 3, 2}, input);
+        Assertions.assertArrayEquals(new int[]{1, 2, 5, 4, 3}, input);
+    }
+
+    @Test
+    public void case02() {
+        int[] input = new int[]{1, 2, 3, 4, 5, 6, 7};
+        findZigZagSequence(input, 7);
+        Assertions.assertArrayEquals(new int[]{1, 2, 3, 7, 6, 5, 4}, input);
     }
 
     private void findZigZagSequence(int [] a, int n) {
         Arrays.sort(a);
-        int mid = (n + 1) / 2;
+        int mid = n / 2;
         int temp = a[mid];
         a[mid] = a[n - 1];
         a[n - 1] = temp;
 
         int st = mid + 1;
-        int ed = n - 1;
+        int ed = n - 2;
         while (st <= ed) {
             temp = a[st];
             a[st] = a[ed];
             a[ed] = temp;
             st = st + 1;
-            ed = ed + 1;
+            ed = ed - 1;
         }
     }
 }
