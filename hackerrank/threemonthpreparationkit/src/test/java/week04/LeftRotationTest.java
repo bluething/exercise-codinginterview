20398cf9684cbb03dbf87dc31ad3ae86ba1695fc
bluething <habib.gunadarma@gmail.com>
2022-01-13 22:22:35 +0700

    Left Rotation 2nd solution
    Juggling algorithm

--- hackerrank/threemonthpreparationkit/src/test/java/week04/LeftRotationTest.java
+++ hackerrank/threemonthpreparationkit/src/test/java/week04/LeftRotationTest.java
@@ -37,7 +37,47 @@ public class LeftRotationTest {
         return arr;
     }
 
+    // Use juggling algorithm
+    // Divide the array in different sets, we have to move elements between sets
+    // The size of set is gcd(d, size of the array)
+    // See more https://stackoverflow.com/questions/23321216/rotating-an-array-using-juggling-algorithm
+    //
+    // For example array of size 6, rotate to left by 3
+    // We have 2 sets with size 3 (gcd(3, 6))
+    // For 1st iteration throw element 1 (i) in the set 1 and throw element 4 (i+d) in the set 2, swap them
+    // Do this for 2nd (2 with 5) and 3rd (3 with 6) iteration
     private List<Integer> rotateLeft2(int d, List<Integer> arr) {
+        int n = arr.size();
+        d %= arr.size();
+        int numOfJuggling = gcd(d, n);
+        for (int i = 0; i < numOfJuggling; i++) {
+            // start element
+            int temp = arr.get(i);
+            int j = i;
+            int k = 0;
+            while (true) {
+                // position to swap
+                k = j + d;
+                // there might be cases we have to rotate the array more than n
+                if (k >= n) {
+                    k -= n;
+                }
+                if (k == i) {
+                    break;
+                }
+                arr.set(j, arr.get(k));
+                j = k;
+            }
+            arr.set(j, temp);
+        }
+
         return arr;
+    }
+    private int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
     }
 }
