6cd8f22cbe647ef95681d735a955ed5780c91fe1
bluething <habib.gunadarma@gmail.com>
2022-01-15 11:30:33 +0700

    GCD using brute force

---
+++ forfun/mathisfun/src/test/java/gcd/GCDUsingBruteForceAlgorithmTest.java
@@ -0,0 +1,28 @@
+package gcd;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GCDUsingBruteForceAlgorithmTest {
+    @Test
+    public void case01() {
+        Assertions.assertEquals(8, gcdUsingBruteForceAlgorithm(64, 40));
+    }
+
+    @Test
+    public void case02() {
+        Assertions.assertEquals(32, gcdUsingBruteForceAlgorithm(32, 64));
+    }
+
+    // O(min(a, b))
+    private int gcdUsingBruteForceAlgorithm(int a, int b) {
+        int gcd = 1;
+        for (int i = 1; i <= a && i <= b; i++) {
+            if (a % i == 0 && b % i == 0) {
+                gcd = i;
+            }
+        }
+
+        return gcd;
+    }
+}
