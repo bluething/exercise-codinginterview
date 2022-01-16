ee3aca26c6567987a52eaa84193c02de288faff1
bluething <habib.gunadarma@gmail.com>
2022-01-16 10:48:17 +0700

    Closest Numbers 2nd solution

--- hackerrank/threemonthpreparationkit/src/test/java/week04/ClosestNumbersTest.java
+++ hackerrank/threemonthpreparationkit/src/test/java/week04/ClosestNumbersTest.java
@@ -31,6 +31,23 @@ public class ClosestNumbersTest {
         Assertions.assertEquals(Arrays.asList(2, 3, 3, 4, 4, 5), closestNumbers(Arrays.asList(5, 4, 3, 2)));
     }
 
+    @Test
+    public void case04() {
+        Assertions.assertEquals(Arrays.asList(-20, 30),
+                closestNumbers2(Arrays.asList(-20, -3916237, -357920, -3620601, 7374819, -7330761, 30, 6246457, -6461594, 266854)));
+    }
+
+    @Test
+    public void case05() {
+        Assertions.assertEquals(Arrays.asList(-520, -470, -20, 30),
+                closestNumbers2(Arrays.asList(-20, -3916237, -357920, -3620601, 7374819, -7330761, 30, 6246457, -6461594, 266854, -520, -470)));
+    }
+
+    @Test
+    public void case06() {
+        Assertions.assertEquals(Arrays.asList(2, 3, 3, 4, 4, 5), closestNumbers2(Arrays.asList(5, 4, 3, 2)));
+    }
+
     // The key is value in arr is unique
     // We only need to calculate abs value from a[i+1]-a[i] then see if the value is the smallest or not
     // I use stack to help store the pairs
@@ -60,5 +77,28 @@ public class ClosestNumbersTest {
         result.sort(Comparator.naturalOrder());
 
         return result;
+    }
+
+    // More efficient, no need to double sort
+    private List<Integer> closestNumbers2(List<Integer> arr) {
+        arr.sort(Comparator.naturalOrder());
+        List<Integer> uniquePairs = new ArrayList<>();
+        int minDistance = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.size() - 1; i++) {
+            int arr1 = arr.get(i);
+            int arr2 = arr.get(i + 1);
+            int difference = Math.abs(arr1 -  arr2);
+            if (difference < minDistance) {
+                minDistance = difference;
+                uniquePairs = new ArrayList<>();
+                uniquePairs.add(arr1);
+                uniquePairs.add(arr2);
+            } else if (difference == minDistance) {
+                uniquePairs.add(arr1);
+                uniquePairs.add(arr2);
+            }
+        }
+
+        return uniquePairs;
     }
 }
