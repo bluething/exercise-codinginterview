b26e22f9ae636f613a5ad1de6977f5486280f974
bluething <habib.gunadarma@gmail.com>
2022-01-16 10:06:25 +0700

    Separate the Numbers algorithm explanation

--- hackerrank/threemonthpreparationkit/src/test/java/week04/SeparateTheNumbersTest.java
+++ hackerrank/threemonthpreparationkit/src/test/java/week04/SeparateTheNumbersTest.java
@@ -7,6 +7,11 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
+// A numeric string, s, is beautiful if it can be split into a sequence of two or more positive integers, a[1], a[2],...,a[n], satisfying the following conditions:
+//
+// 1. a[i] - a[i-1] = 1 for any 1 <= i <= n (i.e., each element in the sequence is  more than the previous element).
+// 2. No a[i]  contains a leading zero. For example, we can split s=10203 into the sequence {1, 02, 03}, but it is not beautiful because 02 and 03 have leading zeroes.
+// 3. The contents of the sequence cannot be rearranged. For example, we can split s=312 into the sequence {3, 1, 2}, but it is not beautiful because it breaks our first constraint (i.e., 1-3 != 1).
 public class SeparateTheNumbersTest {
     private final ByteArrayOutputStream output = new ByteArrayOutputStream();
     private final PrintStream originalOutput = System.out;
@@ -33,6 +38,12 @@ public class SeparateTheNumbersTest {
         Assertions.assertEquals("NO", output.toString());
     }
 
+    // The idea is checking from left to right
+    // Start with 1 digit into s.length() / 2 (compare the half of the string)
+    // For each loop, we check if consecutive string meet the criteria
+    // For example 1st loop (1 digit). Calculate expected next string (+1)
+    // The next consecutive substring will have the same length with expected value
+    // If true, calculate new expected next string (+1) and compare to the next consecutive substring
     private void separateNumbers(String s) {
         boolean result = false;
         String left = "";
