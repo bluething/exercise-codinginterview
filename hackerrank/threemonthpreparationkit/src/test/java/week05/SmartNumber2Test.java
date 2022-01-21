854acfdbba4c769d08898bd8381b6be64aa91ad5
bluething <habib.gunadarma@gmail.com>
2022-01-21 17:10:00 +0700

    Smart Number 2 init problem

---
+++ hackerrank/threemonthpreparationkit/src/test/java/week05/SmartNumber2Test.java
@@ -0,0 +1,34 @@
+package week05;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SmartNumber2Test {
+
+    @Test
+    public void case01() {
+        Assertions.assertTrue(isSmartNumber(1));
+    }
+
+    @Test
+    public void case02() {
+        Assertions.assertFalse(isSmartNumber(2));
+    }
+
+    @Test
+    public void case03() {
+        Assertions.assertFalse(isSmartNumber(7));
+    }
+
+    @Test
+    public void case04() {
+        Assertions.assertTrue(isSmartNumber(169));
+    }
+
+    private boolean isSmartNumber(int num) {
+        int val = (int) Math.sqrt(num);
+        if(num / val == 1)
+            return true;
+        return false;
+    }
+}
