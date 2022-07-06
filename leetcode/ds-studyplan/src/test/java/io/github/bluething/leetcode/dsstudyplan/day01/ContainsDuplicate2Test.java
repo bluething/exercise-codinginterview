4537b3bb535a932a6bf1e66dd704a4be157c3f71
Habib Ali Machpud <habib.machpud@gmail.com>
2022-07-06 09:37:09 +0700

    Update ContainsDuplicate2Test.java
    ContainsDuplicate2Test 2nd solution.
    I need to avoid nested loop. So I use map with array value as key and array index as a value.

--- leetcode/ds-studyplan/src/test/java/io/github/bluething/leetcode/dsstudyplan/day01/ContainsDuplicate2Test.java
+++ leetcode/ds-studyplan/src/test/java/io/github/bluething/leetcode/dsstudyplan/day01/ContainsDuplicate2Test.java
@@ -3,6 +3,9 @@ package io.github.bluething.leetcode.dsstudyplan.day01;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 // Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
 public class ContainsDuplicate2Test {
     @Test
@@ -16,8 +19,23 @@ public class ContainsDuplicate2Test {
     }
 
     @Test
-    public void case03ReturnTrue() {
+    public void case03ReturnFalse() {
         Assertions.assertFalse(containsNearbyDuplicate(new int[]{1,2,3,1,2,3}, 2));
+    }
+
+    @Test
+    public void case01Solution2ReturnTrue() {
+        Assertions.assertTrue(containsNearbyDuplicate2(new int[]{1,2,3,1}, 3));
+    }
+
+    @Test
+    public void case02Solution2ReturnFalse() {
+        Assertions.assertTrue(containsNearbyDuplicate2(new int[]{1,0,1,1}, 1));
+    }
+
+    @Test
+    public void case03Solution2ReturnFalse() {
+        Assertions.assertFalse(containsNearbyDuplicate2(new int[]{1,2,3,1,2,3}, 2));
     }
 
     private boolean containsNearbyDuplicate(int[] nums, int k) {
@@ -29,6 +47,23 @@ public class ContainsDuplicate2Test {
                     break;
                 }
             }
+        }
+
+        return isDuplicate;
+    }
+
+    private boolean containsNearbyDuplicate2(int[] nums, int k) {
+        boolean isDuplicate = false;
+        Map<Integer, Integer> uniqueValues = new HashMap<>();
+        uniqueValues.put(nums[0], 0);
+        for (int i = 1; i < nums.length; i++) {
+            if (uniqueValues.containsKey(nums[i])
+                    && Math.abs(uniqueValues.get(nums[i]) - i) <= k) {
+                isDuplicate = true;
+                break;
+            }
+
+            uniqueValues.put(nums[i], i);
         }
 
         return isDuplicate;
