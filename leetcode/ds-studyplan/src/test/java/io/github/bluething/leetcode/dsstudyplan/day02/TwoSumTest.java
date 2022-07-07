5517bea6d27ab3da4a613c9fb5f92bb55116282f
Habib Ali Machpud <habib.machpud@gmail.com>
2022-07-07 09:17:56 +0700

    Update TwoSumTest.java
    TwoSum 2nd solution with map

--- leetcode/ds-studyplan/src/test/java/io/github/bluething/leetcode/dsstudyplan/day02/TwoSumTest.java
+++ leetcode/ds-studyplan/src/test/java/io/github/bluething/leetcode/dsstudyplan/day02/TwoSumTest.java
@@ -3,6 +3,9 @@ package io.github.bluething.leetcode.dsstudyplan.day02;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 public class TwoSumTest {
     @Test
@@ -20,6 +23,21 @@ public class TwoSumTest {
         Assertions.assertArrayEquals(new int[]{0,1}, twoSum(new int[]{3,3}, 6));
     }
 
+    @Test
+    public void case01Solution2() {
+        Assertions.assertArrayEquals(new int[]{0,1}, twoSum2(new int[]{2,7,11,15}, 9));
+    }
+
+    @Test
+    public void case02Solution2() {
+        Assertions.assertArrayEquals(new int[]{1,2}, twoSum2(new int[]{3,2,4}, 6));
+    }
+
+    @Test
+    public void case03Solution2() {
+        Assertions.assertArrayEquals(new int[]{0,1}, twoSum2(new int[]{3,3}, 6));
+    }
+
     private int[] twoSum(int[] nums, int target) {
         int[] pair = new int[2];
         for (int i = 0; i < nums.length; i++) {
@@ -31,6 +49,25 @@ public class TwoSumTest {
                 }
             }
         }
+        return pair;
+    }
+
+    private int[] twoSum2(int[] nums, int target) {
+        int[] pair = new int[2];
+        Map<Integer, Integer> values = new HashMap<>();
+        values.put(nums[0], 0);
+        int remainder = 0;
+        for (int i = 1; i < nums.length; i++) {
+            remainder = target - nums[i];
+            if (values.containsKey(remainder)) {
+                pair[0] = values.get(remainder);
+                pair[1] = i;
+                break;
+            } else {
+                values.put(nums[i], i);
+            }
+        }
+
         return pair;
     }
 }
