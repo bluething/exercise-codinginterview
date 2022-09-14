7b65c414062f3bc73cb6e241d1a3be29018e6d0e
Habib Ali Machpud <habib.machpud@gmail.com>
2022-09-14 11:02:20 +0700

    Update RunningSumOf1dArrayTest.java
    Running Sum of 1d Array 2nd solution

--- leetcode/leetcode75/src/test/java/io/github/bluething/leetcode/leetcode75/level1/RunningSumOf1dArrayTest.java
+++ leetcode/leetcode75/src/test/java/io/github/bluething/leetcode/leetcode75/level1/RunningSumOf1dArrayTest.java
@@ -29,12 +29,44 @@ public class RunningSumOf1dArrayTest {
         Assertions.assertArrayEquals(expected, runningSum(input));
     }
 
+    @Test
+    public void case04() {
+        int[] input = new int[]{1,2,3,4};
+        int[] expected = new int[]{1,3,6,10};
+        Assertions.assertArrayEquals(expected, runningSum2(input));
+    }
+
+    @Test
+    public void case05() {
+        int[] input = new int[]{1,1,1,1,1};
+        int[] expected = new int[]{1,2,3,4,5};
+        Assertions.assertArrayEquals(expected, runningSum2(input));
+    }
+
+    @Test
+    public void case06() {
+        int[] input = new int[]{3,1,2,10,1};
+        int[] expected = new int[]{3,4,6,16,17};
+        Assertions.assertArrayEquals(expected, runningSum2(input));
+    }
+
     private int[] runningSum(int[] nums) {
         int[] result = new int[nums.length];
         int sum = 0;
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
             result[i] = sum;
+        }
+
+        return result;
+    }
+
+    // without additional temp variable
+    private int[] runningSum2(int[] nums) {
+        int[] result = new int[nums.length];
+        result[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            result[i] = result[i-1] + nums[i];
         }
 
         return result;
