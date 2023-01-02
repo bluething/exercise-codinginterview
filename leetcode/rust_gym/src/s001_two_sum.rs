1a6b51efa4a7492fb2d4e0daae7c88ab536c8d67
Habib Ali Machpud <habib.machpud@gmail.com>
2023-01-02 17:04:56 +0700

    2nd solution

--- leetcode/rust_gym/src/s001_two_sum.rs
+++ leetcode/rust_gym/src/s001_two_sum.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 struct Solution {}
 
 impl Solution {
@@ -11,6 +13,19 @@ impl Solution {
             }
         }
 
+        vec![]
+    }
+
+    pub fn two_sum_with_hashmap(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map = HashMap::with_capacity(nums.len());
+        for (idx, value) in  nums.iter().enumerate() {
+            match map.get(&(target - value)) {
+                None => {
+                    map.insert(value, idx);
+                }
+                Some(second_idx) => return vec![*second_idx as i32, idx as i32],
+            }
+        }
         vec![]
     }
 }
@@ -33,4 +48,24 @@ fn test3() {
 #[test]
 fn test4() {
     assert_eq!(Solution::two_sum(vec![2,5,5,11], 10), vec![1, 2]);
+}
+
+#[test]
+fn test5() {
+    assert_eq!(Solution::two_sum_with_hashmap(vec![2, 7, 11, 15], 9), vec![0, 1]);
+}
+
+#[test]
+fn test6() {
+    assert_eq!(Solution::two_sum_with_hashmap(vec![2, 7, 11, 15], 9999), vec![]);
+}
+
+#[test]
+fn test7() {
+    assert_eq!(Solution::two_sum_with_hashmap(vec![3, 2, 4], 6), vec![1, 2]);
+}
+
+#[test]
+fn test8() {
+    assert_eq!(Solution::two_sum_with_hashmap(vec![2,5,5,11], 10), vec![1, 2]);
 }
