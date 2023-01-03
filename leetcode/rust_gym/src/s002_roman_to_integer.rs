bc0df56006079560fd055a49b6c1c5cffbf5e98d
Habib Ali Machpud <habib.machpud@gmail.com>
2023-01-03 22:52:50 +0700

    roman to integer 1st solution
    Rev loop

--- leetcode/rust_gym/src/s002_roman_to_integer.rs
+++ leetcode/rust_gym/src/s002_roman_to_integer.rs
@@ -1,9 +1,45 @@
 // https://leetcode.com/problems/roman-to-integer/
 
+use std::collections::HashMap;
+
 struct Solution {}
 
 impl Solution {
     pub fn roman_to_int(s: String) -> i32 {
-        0
+        let values = HashMap::from([
+            (b'\0', 0),
+            (b'I', 1),
+            (b'V', 5),
+            (b'X', 10),
+            (b'L', 50),
+            (b'C', 100),
+            (b'D', 500),
+            (b'M', 1000)
+        ]);
+
+        let mut result = 0;
+        let mut prev = b'\0';
+        for roman in s.bytes().rev() {
+            let value = values[&roman];
+            if values[&prev] <= value {
+                result += value;
+            } else {
+                result -= value;
+            }
+
+            prev = roman;
+        }
+
+        result
     }
+}
+
+#[test]
+fn test1() {
+    assert_eq!(Solution::roman_to_int(String::from("III")), 3);
+}
+
+#[test]
+fn test2() {
+    assert_eq!(Solution::roman_to_int(String::from("IV")), 4);
 }
