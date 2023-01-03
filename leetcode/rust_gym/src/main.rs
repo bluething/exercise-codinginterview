bb89a73b75dd76c690b1a444b2eda2cd4d02f149
Habib Ali Machpud <habib.machpud@gmail.com>
2023-01-03 22:14:44 +0700

    Add roman-to-integer

--- leetcode/rust_gym/src/main.rs
+++ leetcode/rust_gym/src/main.rs
@@ -1,4 +1,5 @@
 mod s001_two_sum;
+mod s002_roman_to_integer;
 
 fn main() {
     println!("Hello, world!");
---
+++ leetcode/rust_gym/src/s002_roman_to_integer.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/roman-to-integer/
+
+struct Solution {}
+
+impl Solution {
+    pub fn roman_to_int(s: String) -> i32 {
+        0
+    }
+}
