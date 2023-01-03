39d25fcc0741c364fe242f58273ca7c9dd230501
Habib Ali Machpud <habib.machpud@gmail.com>
2023-01-03 23:07:27 +0700

    roman to integer 2nd solution
    Normal loop

--- leetcode/rust_gym/src/s002_roman_to_integer.rs
+++ leetcode/rust_gym/src/s002_roman_to_integer.rs
@@ -32,6 +32,35 @@ impl Solution {
 
         result
     }
+
+    pub fn roman_to_int_rev_loop(s: String) -> i32 {
+        let mut result = 0;
+        let mut prev = 0;
+        let mut current : i32;
+        for roman in s.chars() {
+            match roman {
+                'M' => current = 1000,
+                'D' => current = 500,
+                'C' => current = 100,
+                'L' => current = 50,
+                'X' => current = 10,
+                'V' => current = 5,
+                'I' => current = 1,
+                _ => current = 0,
+            }
+            if current > prev {
+                // try to get negative value from previous, for example CXL
+                // when we enter L we will have
+                // result = 110 and current = 10
+                // before we add L to the result, we subtract the result with 2*10
+                result -= prev * 2;
+            }
+            prev = current;
+            result += current;
+        }
+
+        result
+    }
 }
 
 #[test]
@@ -42,4 +71,14 @@ fn test1() {
 #[test]
 fn test2() {
     assert_eq!(Solution::roman_to_int(String::from("IV")), 4);
+}
+
+#[test]
+fn test3() {
+    assert_eq!(Solution::roman_to_int_rev_loop(String::from("III")), 3);
+}
+
+#[test]
+fn test4() {
+    assert_eq!(Solution::roman_to_int_rev_loop(String::from("IV")), 4);
 }
